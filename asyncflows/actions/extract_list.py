4c29045fbaabc40ad8bdda11189c1f52e3dd487a
Rafael Irgolic <hello@irgolic.com>
2024-05-15 10:07:01 +0100

    extract_list: init action

---
+++ asyncflows/actions/extract_list.py
@@ -0,0 +1,46 @@
+from typing import Optional, Literal, assert_never
+
+from pydantic import BaseModel
+
+from asyncflows.actions.base import Action
+
+LIST_FORMAT = Literal["comma", "newline", "space", "bullet points"]
+
+
+class Inputs(BaseModel):
+    text: str
+    valid_values: Optional[list[str]] = None
+    list_format: LIST_FORMAT = "bullet points"
+
+
+class Outputs(BaseModel):
+    results: list[str]
+
+
+class ExtractList(Action[Inputs, Outputs]):
+    name = "extract_list"
+
+    async def run(self, inputs: Inputs) -> Outputs:
+        if inputs.list_format == "comma":
+            candidates = inputs.text.split(",")
+        elif inputs.list_format == "newline":
+            candidates = inputs.text.split("\n")
+        elif inputs.list_format == "space":
+            candidates = inputs.text.split(" ")
+        elif inputs.list_format == "bullet points":
+            candidates = inputs.text.split("-")
+        else:
+            assert_never(inputs.list_format)
+            # raise ValueError(f"Invalid list_format: {inputs.list_format}")
+
+        choices = []
+        for candidate in candidates:
+            candidate = candidate.strip()
+            if not candidate:
+                continue
+            if inputs.valid_values and candidate not in inputs.valid_values:
+                continue
+            choices.append(candidate)
+        return Outputs(
+            results=choices,
+        )
