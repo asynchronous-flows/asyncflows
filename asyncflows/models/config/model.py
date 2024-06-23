ae769a5073b80934de3f383f410fb9c7e8cde0a9
Rafael Irgolic <hello@irgolic.com>
2024-06-23 23:06:02 +0100

    model: Add sonnet-3.5

--- asyncflows/models/config/model.py
+++ asyncflows/models/config/model.py
@@ -50,6 +50,7 @@ ModelType = (
     # anthropic models
     Annotated[
         Literal[
+            "claude-3-5-sonnet-20240620",
             "claude-3-haiku-20240307",
             "claude-3-opus-20240229",
             "claude-3-sonnet-20240229",
